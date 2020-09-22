b9996380c486e61ecb807340f3efc5990dbcbfb3
Jack Zhou <J.Zhou@televic.com>
2020-09-22 17:18:33 +0800

    commit

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -3,13 +3,30 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 func Hello(name string) (string, error) {
+	// If no name was given, return an error with a message.
 	if name == "" {
-		return "", errors.New("empty name")
+		return name, errors.New("empty name")
+	}
+	// Create a message using a random format.
+	message := fmt.Sprintf(randomFormat(), name)
+	return message, nil
+}
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func randomFormat() string {
+	formats := []string{
+		"Hi, %v. Welcome!",
+		"Great to see you, %v!",
+		"Hail, %v! Well met!",
 	}
 
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
-	return message, nil
+	return formats[rand.Intn(len(formats))]
 }
